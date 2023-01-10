3e4b89dee149b8ee721f8a8fd88c5af23a068dc1
Artem Prokhorov <electron3djavadev@gmail.com>
2023-01-10 11:55:24 +0300

    little fixes

--- src/main/java/com/repository/Main.java
+++ src/main/java/com/repository/Main.java
@@ -2,8 +2,6 @@ package com.repository;
 
 import com.repository.RandomChooserWhoDriveToLunch.RandomChooser;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         RandomChooser randomChooser = new RandomChooser();
